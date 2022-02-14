613745851635de582d1060d1e4037198748d5fa1
Rogelio <galiciar0513@gmail.com>
2022-02-14 14:22:49 -0600

    compareGuess() function created

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-// This function will be called at the start of each round in order to generate the new secret target number
-const generateTarget = () => {
-   let randomInt =  Math.floor(Math.random()) * 9;
-   return randomInt;
-}
+// generate the new secret target number
+const generateTarget = () => Math.floor(Math.random() * 9);
 
 console.log(generateTarget());
 
+// generate the new secret target number 
+const compareGuesses = (user, computer, targetNumber) => {
+    
+}

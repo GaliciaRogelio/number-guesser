157254b18e4b0310959e8ef2dce570ea810f6fbd
Rogelio <galiciar0513@gmail.com>
2022-02-14 17:05:56 -0600

    updated code to add conditions to game.

--- script.js
+++ script.js
@@ -6,22 +6,28 @@ let currentRoundNumber = 1;
 // generate the new secret target number
 const generateTarget = () => Math.floor(Math.random() * 10);
 
-console.log(generateTarget());
+// console.log(generateTarget());
+
+// Function to get the absolute distance without duplicating code.
+const getAbsoluteDistance = (guess, target) => Math.abs(guess - target);
 
 // generate the new secret target number
 const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+    // Alert if the user guess is out of range.
+    if (userGuess < 0 || userGuess > 9) {
+        alert('Invalid input!, number must be between 0 - 9.');
+    };
   // target number will be a random number. it hets connected to generateTarget().
-  targetNumber = generateTarget();
+//   targetNumber = generateTarget();
   // Use Math.abs() to get the difference between human and target. Same for computer and target.
-  const humanVar = Math.abs(userGuess - targetNumber);
-  const compuVar = Math.abs(computerGuess - targetNumber);
-
-  if (humanVar < compuVar) {
+//   const humanVar = Math.abs(userGuess - targetNumber);
+//   const compuVar = Math.abs(computerGuess - targetNumber);
+// update userGuess by doing modulus 10 to correct invalid entries and make them in range.
+const newUserGuess = userGuess % 10;
+  if (getAbsoluteDistance(newUserGuess, targetNumber) <= getAbsoluteDistance(computerGuess, targetNumber)) {
     return true;
-  } else if (compuVar < humanVar) {
+  } else {
     return false;
-  } else if (compuVar === humanVar) {
-    return true;
   }
 };
 

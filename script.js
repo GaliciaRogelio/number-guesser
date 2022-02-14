df64a4b2e50bd32eecf9495f3ece6e8bc528fc31
Rogelio <galiciar0513@gmail.com>
2022-02-14 15:31:06 -0600

    added updateScore()

--- script.js
+++ script.js
@@ -4,11 +4,36 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 // generate the new secret target number
-const generateTarget = () => Math.floor(Math.random() * 9);
+const generateTarget = () => Math.floor(Math.random() * 10);
 
 console.log(generateTarget());
 
-// generate the new secret target number 
-const compareGuesses = (user, computer, targetNumber) => {
-    
-}
+// generate the new secret target number
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  // target number will be a random number. it hets connected to generateTarget().
+  targetNumber = generateTarget();
+  // Use Math.abs() to get the difference between human and target. Same for computer and target.
+  const human = Math.abs(userGuess - targetNumber);
+  const compu = Math.abs(computerGuess - targetNumber);
+
+  if (human < compu) {
+    return true;
+  } else if (compu < human) {
+    return false;
+  } else if (compu === human) {
+    return true;
+  }
+};
+
+// console.log(compareGuesses(1,3));
+
+// Increase the winner's score after each round.
+const updateScore = (winner) => {
+  let humanScore;
+  let computerScore;
+  if (winner === "human") {
+    humanScore + 1;
+  } else if (winner === "computer") {
+    computerScore + 1;
+  }
+};
